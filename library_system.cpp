54304d3f19d70f2b1e96b3f41097d7229a32e786
Ashwin <ashwinrockon123@gmail.com>
2021-04-23 19:01:12 +0530

    Print users, who all borrowed a book -- features(total 9) added

--- library_system.cpp
+++ library_system.cpp
@@ -70,6 +70,18 @@ struct user{
         borrowed = 0;
     }
 
+    void print()
+    {
+        cout<<"user: "<<name<<", id : "<<id<<", Borrowed Books: ";
+
+        // If the user has not borrowed any books, it must show "Nil"
+        if(borrowed == 0)   cout<<"Nil";
+        
+
+        for(int i = 0; i < borrowed; ++i)
+            cout<<borrowed_books[i]<<" ";
+    }
+
     void add(int _id, string _name)
     {
         id = _id;
@@ -87,6 +99,14 @@ struct user{
     void return_book()
     {
         --borrowed;
+    }
+
+    bool is_present(string book_name)
+    {
+        for(int i = 0; i < borrowed; ++i)
+            if(borrowed_books[i] == book_name)
+                return true;
+        return false;
     }
 };
 
@@ -110,7 +130,13 @@ void search_by_prefix(book b[])
 
     for(int i = 0; i < book_idx; ++i)
         if(b[i].is_prefix(prefix))
+        {
             cout<<b[i].name<<"\n";
+            return;
+        }
+
+    // if there are no books
+    cout<<"Sorry there were no search results, check your spelling or try different prefixes";
 }
 
 // used to print books after they are sorted
@@ -132,6 +158,36 @@ bool cmp_by_name(book &b1, book &b2)
     return b1.name < b2.name;
 }
 
+// for getting the index of the object which has the given book
+int book_exists(book b[], string book_name)
+{
+    int idx;
+    for(idx = 0; idx < book_idx; ++idx)
+    {
+        if(b[idx].name == book_name)
+            break;
+    }
+    
+    if(idx == book_idx)
+        return -1;
+    
+    return idx;
+}
+// for getting the index of the object which has the given user
+int user_exists(user u[], string user_name)
+{
+    int idx;
+    for(idx = 0; idx < user_idx; ++idx)
+    {
+        if(u[idx].name == user_name)
+            break;
+    }
+
+    if(idx == user_idx)
+       return -1;
+    return idx;
+}
+
 void add_user(user &u)
 {
     int id;
@@ -151,42 +207,32 @@ void user_borrow_book(book b[], user u[])
     cout<<"Enter username and Book name: ";
     cin>>user_name>>book_name;
 
-    // search if book is present in the library
-    int i;
-    for(i = 0; i < book_idx; ++i)
-    {
-        if(b[i].name == book_name)
-            break;
-    }
-    
-    if(i == book_idx)
+    // search if book Exists return its index
+    int book_exists_idx = book_exists(b, book_name);
+
+    if(book_exists_idx == -1)
     {
         cout<<"Book not found :(";
         return;
     }
 
-    // search if user is present in the library
-    int j;
-    for(j = 0; j < user_idx; ++j)
-    {
-        if(u[j].name == user_name)
-            break;
-    }
-
-    if(j == user_idx)
+    // search if user Exists and return its index
+    int user_exists_idx = user_exists(u, user_name);
+    
+    if(user_exists_idx == -1)
     {
         cout<<"User not found :(";
         return;
     }
-    
+
     // Borrow
     /*
     1. Check if book is present
-    2. Check is user can borrow this book
+    2. Check if user can borrow this book
     */
-    if(b[i].borrow_book())
+    if(b[book_exists_idx].borrow_book())
     {
-        if(u[j].borrow_book(book_name))
+        if(u[user_exists_idx].borrow_book(book_name))
             cout<<user_name<<" has successfully borrowed "<<book_name;
         else
             cout<<user_name<<" cant borrow this book as he/she has exausted the borrowing limit";
@@ -202,59 +248,72 @@ void user_return_book(book b[], user u[])
     cin>>user_name>>book_name;
 
     // search if book is present in the library
-    int i;
-    for(i = 0; i < book_idx; ++i)
-    {
-        if(b[i].name == book_name)
-            break;
-    }
-    
-    if(i == book_idx)
+    int book_exists_idx = book_exists(b, book_name);
+
+    if(book_exists_idx == -1)
     {
         cout<<"Book not found :(";
         return;
     }
 
     // search if user is present in the library
-    int j;
-    for(j = 0; j < user_idx; ++j)
-    {
-        
-        if(u[j].name == user_name)
-        {
-            // Search if user has borrowed the book
-            int k;
-            for(k = 0; k < u[j].borrowed; ++k)
-            {
-                if(u[j].borrowed_books[k] == book_name)
-                    break;
-            }
-            if(k == u[j].borrowed)
-            {
-                cout<<"User has not borrowed this book, please check again";
-                return;
-            }
-            else
-            {
-                // Return the book
-                for(int a = k; a < u[j].borrowed; ++a)
-                {
-                    u[j].borrowed_books[a] = u[j].borrowed_books[a + 1];
-                }
-                u[j].return_book();
-                b[i].return_book();
-            }
-        }
-            break;
-    }
-
-    if(j == user_idx)
+    int user_exists_idx = user_exists(u, user_name);
+    
+    if(user_exists_idx == -1)
     {
         cout<<"User not found :(";
         return;
     }
+
+    // Search if user has borrowed the book
+    int k;
+    for(k = 0; k < u[user_exists_idx].borrowed; ++k)
+    {
+        if(u[user_exists_idx].borrowed_books[k] == book_name)
+            break;
+    }
+
+    if(k == u[user_exists_idx].borrowed)
+    {
+        cout<<"User has not borrowed this book, please check again";
+        return;
+    }
+
     else
-        cout<<book_name<<" returned successfully by "<<user_name;
+    {
+        // Return the book
+        for(int a = k; a < u[user_exists_idx].borrowed; ++a)
+        {
+            u[user_exists_idx].borrowed_books[a] = u[user_exists_idx].borrowed_books[a + 1];
+        }
+        u[user_exists_idx].return_book();
+        b[book_exists_idx].return_book();
+    }
+    cout<<book_name<<" returned successfully by "<<user_name;
+}
+
+void print_who_borrowed_book_by_name(book b[], user u[])
+{
+    string book_name;
+    cout<<"Enter the book name: ";
+    cin>>book_name;
+
+    //Search if book exists
+    int book_exists_idx = book_exists(b, book_name);
+
+    // Search if a user has that book borrowed
+    for(int i = 0; i < user_idx; ++i)
+        if(u[i].is_present(book_name))
+            cout<<u[i].name<<" ";
+}
+
+void print_users(user u[])
+{
+    for(int i = 0; i < user_idx; ++i)
+    {
+        u[i].print();
+        cout<<"\n";
+    }
 }
 
 int main()
@@ -265,7 +324,7 @@ int main()
     cout<<"\t\tLIBRARY SYSTEM\n\n";
     
     while(true){
-        cout<<" 1. Add a book\n 2. Search books by prefix\n 3. Print Library by id\n 4. Print Library by name\n 5. Add a user\n 6 Borrow a book\n 7. Return a book\n 8. Exit\nChoose your option: ";
+        cout<<" 1. Add a book\n 2. Search books by prefix\n 3. Print Library by id\n 4. Print Library by name\n 5. Add a user\n 6. Borrow a book\n 7. Return a book\n 8. Who all borrowed a book?\n 9. Print Users\n 10. Exit\n Choose your option: ";
         int option;
         cin>>option;
 
@@ -294,7 +353,14 @@ int main()
             case 7: user_return_book(books, users);
             break;
 
-            case 8: return 0;
+            case 8: print_who_borrowed_book_by_name(books, users);
+            break;
+
+            case 9: print_users(users);
+            break;
+
+            case 10: return 0;
+
 
             default: cout<<"Invalid Option, Try again";
 
